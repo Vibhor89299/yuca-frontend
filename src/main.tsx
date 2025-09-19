fc23232db163bb8b8beeae61e92726bba8efd6a2
akthakur04 <akshay.181004@gmail.com>
2025-09-19 15:49:43 +0530

    Product page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App.tsx';
 import './index.css';
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <HelmetProvider>
       <App />
+      </HelmetProvider>
     </Provider>
   </StrictMode>
 );
