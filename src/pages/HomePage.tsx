cceba7ca32a83f07e22b317a76e52429933dc802
akthakur04 <akshay.181004@gmail.com>
2025-09-22 12:06:56 +0530

    public folder

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,12 @@
 import { HeroSection } from '@/components/hero/HeroSection';
-// import { CategorySection } from '@/components/category/CategorySection';
 import { KoshaCollection } from '@/components/featured/FeaturedProducts';
 
 export function HomePage() {
   return (
     <div className="min-h-screen">
       <HeroSection />
-  {/* <CategorySection /> */}
       <KoshaCollection />
     </div>
   );
 }
+
