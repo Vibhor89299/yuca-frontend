7ae1f696d7ff28e5b19769d1eaeffc28ba648695
VibhorR <vibhorrawat95@gmail.com>
2025-10-26 12:46:45 +0530

    added new banner video fr the banner

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -73,7 +73,7 @@ export function Header() {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out ${"translate-y-0 bg-kimber/95 backdrop-blur-lg shadow-lg border-b border-oak/50"}`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out ${"translate-y-0 bg-kimber/95 backdrop-blur-lg shadow-lg"}`}
     >
       <div className="container mx-auto px-4 ">
         {/* Compact Header */}
